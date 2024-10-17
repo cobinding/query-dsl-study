8929ed3c20f0411c8e2f6f21ffb7f6337e40461b
cobinding <cntpqls0218@gmail.com>
2024-10-18 03:45:12 +0900

    [BUILD] queryDSL 버전 문제 해결 및 빌드 설정 완료

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
-class QuerydslApplicationTests {
+@Transactional
+@EnableWebMvc
+@Commit // 테스트 롤백하지 않도록 설정
+public class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        // 쿼리와 관련된 건 모두 Q 타입으로 정의하고, DB에 넣기
+        QHello qHello = QHello.hello;
+        Hello result = query
+            .selectFrom(qHello)
+            .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+
+    }
 }
